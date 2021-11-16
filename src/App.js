214f870f41ea7c69aa43458278327cd7074595fe
theboss-micro <mhiri.hichem.8@gmail.com>
2021-11-16 15:58:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import {Nav, Navbar, NavDropdown, Container} from "react-bootstrap"
 import './App.css';
-
+import logo from "../src/components/logo.jpg"
+import Intro from './components/Intro'
+import Main from "./components/main"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar bg="bg" className="hello" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home"><img src={logo} /></Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#service">SERVICES</Nav.Link>
+        <Nav.Link href="#A-propos">A propos</Nav.Link>
+        <Nav.Link href="#Galerie">Galerie</Nav.Link>
+        
+        <NavDropdown title="contact" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
+<Intro />
+<Main />
+
     </div>
   );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../logo.svg'
+
+const Intro = () => {
+    return (
+        <div>
+            <img src={logo} />
+        </div>
+    )
+}
+
+export default Intro
